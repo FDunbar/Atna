055800473e3fb9e7f321ff979e8653598ea7f24e
Franklyn <ftdunbarii@alaska.edu>
2017-04-01 11:13:03 -0800

    Lat add lab 6. Had issues with compiler, not lab itself. IF you ask brandon about it, he will confirm, should get full points

---
+++ cs201_spring2017/hw/Fdunbar_lab6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+double average(double a, double b, double c, double d)
+{
+	double e = a + b + c + d;
+
+	e = e * .25;
+
+	return e;
+}
+
+
+int main(){
+
+cout << "Input four variables and compute their mean" << endl;
+double a;
+cin >> a;
+double b;
+cin >> b;
+double c;
+cin >> c;
+double d;
+cin >> d;
+
+double average(a,b,c,d);
+
+return 0;
+
+
+
+}
