b8396f17ca87f56db1bfda6724c5043e8f9a6ad2
Franklyn <ftdunbarii@alaska.edu>
2017-04-05 11:32:24 -0800

    All work untill now, including labs

---
+++ cs201_spring2017/hw/hw04/F_dunbar_cs201_lab3-27.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include "frankfunc.h"
+using namespace std;
+
+int main(){
+
+vector<double> v ={1,2,3,5,6,7,8,9,22};
+
+
+cout<<vecsum(v)<<endl;
+
+cout<<meanvec(v)<<endl;
+
+
+return 0;
+}
